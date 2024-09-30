9e75787e0e3a613d5153d0d0a74a784d34bc92b6
Lauri Lehtinen <lauri.lehtinen7@metropolia.fi>
2024-09-30 14:31:21 +0300

    project created with dummys

--- flyAraoundTheWorld/game.py
+++ flyAraoundTheWorld/game.py
@@ -1,4 +1,7 @@
 #luokka yllapitamaan pelisilmukkaa
+from formatter import NullWriter
+
+
 class Game:
     def __init__(self):
         # staattiset muuttujat listoille, jotka sisaltavat pelikulkureitit
@@ -15,12 +18,31 @@ class Game:
         hintaR = 0#hinta maan vaihdolle
         hintaY = 0#hinta yopymiselle
 
-    #metodi laskemaan pelin pistesaldoa
+    #pelisilmukka
+    def game(self):
+        while True:
+            DUMMY = 0
+            #kutsu UIsta pelin aloitus sivu
 
     #metodi lentamiselle
+    def fly(self, icao):
+        DUMMY = 0
+        #muuta pelaajan sijainti annettuun arvoon
+        #laske lennon hinta ja erota se varoista
+        #siirra aika lennon keston verran
 
     #metodi yopymiselle
+    def sleep(self):
+        DUMMY = 0
+        #siirra aikaa 8h
+        #lisaa varoja pelaajalle
 
     #metodi odottamiselle
+    def wait(self):
+        DUMMY = 0
+        #siirra aikaa eteenpain ensimmaisen lennon lahtoaikaan
 
-    #pelisilmukka
+    # metodi laskemaan pelin pistesaldoa
+    def finalScore(self):
+        DUMMY = 0
+        #laske pelin lopputulos tallennetusta datasta
