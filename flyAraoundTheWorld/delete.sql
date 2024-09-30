1d537d7e6793f1f300d27c5a14794f02a37bfb34
Niittyvilla2 <milokatu@metropolia.fi>
2024-09-30 14:50:12 +0300

    Create delete.sql

---
+++ flyAraoundTheWorld/delete.sql
@@ -0,0 +1,28 @@
+create table high_score(
+id int not null auto_increment,
+player_name varchar(40) default null,
+time int default null,
+money int default null,
+distance int default null,
+flight_count int default null,
+country_count int default null,
+continent_count int default null,
+primary key(id)
+);
+delete from airport
+where type = "heliport";
+
+delete from airport
+where type = "balloonport";
+
+delete from airport
+where type = "small_airport";
+
+delete from airport
+where type = "medium_airport"
+
+delete from airport
+where type = "seaplane_base"
+
+delete from airport
+where type = "closed";
