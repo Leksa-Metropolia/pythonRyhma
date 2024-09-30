85afff9775fa33c4000794dbde35d6105162dd3a
Lauri Lehtinen <lauri.lehtinen7@metropolia.fi>
2024-09-30 15:18:42 +0300

    expanding dummys for gameUI

--- flyAraoundTheWorld/gameUI.py
+++ flyAraoundTheWorld/gameUI.py
@@ -1,14 +1,56 @@
-#luokka ja metodit pelin komentokehotteen nayttoon ja hallintaan
-    #metodi nayttamaan pelin aloitus sivun
+#metodi nayttamaan pelin aloitus sivun
+def gameMainMenu():
+    DUMMY = 0
+    #nayta vaitoehdot pelaajalle: pelin aloitus, high score lista ja lopeta ohjelma
 
-    #metodi nayttamaan pelin kulun sivun
+#metodi nayttamaan pelin kulun sivun
+def gameActiveMenu():
+    DUMMY =0
+    #nayta pelaajan tiedot
+    #nayta vaihtoehdot pelaajalle:
+    #lenna: avaa syoteenoton lentokohteen valinnaelle
+    #yovy
+    #odota
+    #lopeta peli ja palaa paa valikkoon
+    #ottaa vastaan kayttajasyotteen ja kutsuu oikeaa funktiota
 
-    #metodi nayttamaan pelin loppu sivun
+#metodi nayttamaan pelin loppu sivun
+def gameEndSuccess():
+    DUMMY = 0
+    #nayta pelaajan kaymien valtioiden, mannerten ja lentokenttien maarat
+    #nayta pelaajan pistetulos
+    #siirra pelaaja paavalikkoon ENTERia painamalla
 
-    #metodi tarkistamaan pelaajan syotteen ja kutsumaan edellisen syote rivin uudelleen jos ei validi syote pelaajalta
+def gameEndFailure():
+    DUMMY = 0
+    #nayta pelaajan kaymien valtioiden, mannerten ja lentokenttien maarat
+    #siirra pelaaja paavalikkoon ENTERia painamalla
 
-    #metodit ottamaan vastaan pelaajansyotteet
+#metodi tarkistamaan pelaajan syotteen ja kutsumaan edellisen syote rivin uudelleen jos ei validi syote pelaajalta
+def inputCheck(input, origin):
+    DUMMY = 0
+    #tarkistaa, etta syote on odotettu ja pyytaa syotteen udelleen jos ei ole
 
-    #metodi nayttamaan high score listan
+#metodit ottamaan vastaan pelaajansyotteet
+def selectFlight(playerLocation):
+    DUMMY = 0
+    #nayttaa pelaajalle mantereet minne pystytaan lentamaan
+    #ottaa vastaan mantereen minne pelaaja haluaa lentaa
+    #nayttaa pelaajalle maat minne pystytaan lentamaan valitulla mantereella
+    #ottaa vastaan maan minne pelaaja haluaa lentaa
+    #nayttaa pelaajalle lentokentat joille pystytaan lentamaan
+    #ottaa vastaan lentokentan jolle pelaaja haluaa lentaa
+    #lentaa pelaaja valitulle kentalle
+    #palaa pelin kulkuvalikkoon
 
-    #metodi tyhjentamaan komentokehoteeen
+#metodi nayttamaan high score listan
+def showHS(origin):
+    DUMMY = 0
+    #kysyy pelaajalta minka reitin parhaat tulokset halutaan nahda
+    #nayttaa pelaajalle reitin 10 parasta tulosta
+    #palaa ENTERia painamalla edelliseen valikkoon
+
+#metodi tyhjentamaan komentokehoteeen
+def clearTerminal(menu):
+    DUMMY = 0
+    #tyhjentaa terminaalin tekstista ennen seuraavan valikon tulostamista
