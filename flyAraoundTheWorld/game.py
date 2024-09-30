451bfc57eba61afd523865539d33266ee4548263
Lauri Lehtinen <lauri.lehtinen7@metropolia.fi>
2024-09-30 14:02:36 +0300

    project created with dummys

--- flyAraoundTheWorld/game.py
+++ flyAraoundTheWorld/game.py
@@ -1,6 +1,19 @@
 #luokka yllapitamaan pelisilmukkaa
-    #staattiset muuttujat listoille, jotka sisaltavat pelikulkureitit
-    #staattiset muuttujat lentokilometrin, mantereen ylityksen, maan ylityksen ja yopymisen hinnalle
+class Game:
+    def __init__(self):
+        # staattiset muuttujat listoille, jotka sisaltavat pelikulkureitit
+        l1 = []
+        l2 = []
+        l3 = []
+        l4 = []
+        l5 = []
+        l6 = []
+        l7 = []
+        l8 = []
+        hintaLK = 0#hinta lentokilometrille
+        hintaM = 0#hinta mantereen vaihdolle
+        hintaR = 0#hinta maan vaihdolle
+        hintaY = 0#hinta yopymiselle
 
     #metodi laskemaan pelin pistesaldoa
 
