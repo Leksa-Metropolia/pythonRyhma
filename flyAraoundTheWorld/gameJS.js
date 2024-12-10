567e4bc9c462da493fbbcf11be08dbcfdd7b4d95
l3ksa <lauri.lehtinen7@metropolia.fi>
2024-12-10 19:02:58 +0200

    2.0

--- flyAraoundTheWorld/gameJS.js
+++ flyAraoundTheWorld/gameJS.js
@@ -3,6 +3,10 @@ let valid_locations = []
 let continents = []
 let countries = []
 let airports = []
+let sizes = []
+let small = []
+let medium = []
+let large = []
 
 function start_game() {
     let player_name = document.getElementById('player_name').value
@@ -13,6 +17,8 @@ function start_game() {
         contentType: 'application/json',
         data: JSON.stringify({'player_name': player_name, 'route': route}),
         success: function (response) {
+            valid_flights()
+            update()
             //kitjoita pätkä siitrymään aloitus menusta pelin pää menuhun
         },
         error: function (error) {
@@ -40,18 +46,32 @@ function fly() {
         contentType: 'application/json',
         data: JSON.stringify({'icao': icao}),
         success: function (response) {
-            //kitjoita pätkä siitrymään aloitus menusta pelin pää menuhun
+            valid_flights()
+            update()
+            //siirry pelin pää menuhun
         },
         error: function (error) {
             console.log(error)
         }})
 }
 
+function wait() {
+    $.get('/wait', function(data, status) {
+    valid_flights()
+    update()})
+}
+
+function sleep() {
+    $.get('/sleep', function(data, status) {
+    valid_flights()
+    update()})
+}
+
 function list_continents() {
     continents = []
     for (let airport in valid_locations) {
         if (!continents.includes(airport['continent'])) {
-            continents.append(airport['cntinent'])
+            continents.append(airport['continent'])
         }
     }
 }
@@ -69,7 +89,27 @@ function list_airports(country) {
     airports = []
     for (let airport in valid_locations) {
         if (airport['country'] === country) {
+            if (airport['size'] === 'small') {
+                small.append({'icao': airport['icao'], 'name': airport['name']})
+            } else if (airport['size'] === 'medium') {
+                medium.append({'icao': airport['icao'], 'name': airport['name']})
+            } else if (airport['size'] === 'large') {
+                large.append({'icao': airport['icao'], 'name': airport['name']})
+            }
             airports.append({'icao': airport['icao'], 'name': airport['name']})
         }
+    }
+}
+
+function list_size() {
+    sizes = []
+    if (small.length > 0) {
+        sizes.append('small')
+    }
+    if (medium.length > 0) {
+        sizes.append('medium')
+    }
+    if (large.length > 0) {
+        sizes.append('large')
     }
 }
