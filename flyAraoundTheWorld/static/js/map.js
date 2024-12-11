7d4eccc366ffe6ad8ce33683780ba6c80a246997
Meeri Hurtta <Meeri.Hurtta@metropolia.fi>
2024-12-11 19:09:31 +0200

    update

--- flyAraoundTheWorld/static/js/map.js
+++ flyAraoundTheWorld/static/js/map.js
@@ -3,14 +3,16 @@
 // Ladataan Google Maps -API
 function loadGoogleMapsAPI(callback) {
     const script = document.createElement("script");
-    script.src = "https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY";
+    script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBmovX9_tGA7WBQhV9OQQOwkciOxRySghY";
     script.async = true;
     script.defer = true;
     script.onload = callback;
     document.head.appendChild(script);
 }
 
-// Alusta kartta ja markkerit
+// Viiva pelaajan sijainnin ja valitun lentokentän välille
+let flightPath;
+
 function initMap() {
     // Haetaan kartan kontti
     const mapContainer = document.getElementById("map");
@@ -31,29 +33,68 @@ function initMap() {
                 title: `${airport.name} (${airport.icao})`, // Markkerin otsikko (näkymä tooltipissä)
             });
 
-            // Jos halutaan niin tällä saadaan infoikkunaa markkereille
-             const infoWindow = new google.maps.InfoWindow({
+            // Lisätään infoikkuna markkereille
+            const infoWindow = new google.maps.InfoWindow({
                 content: `<div>
-                            <h3>${location.name}</h3>
-                            <p>Maa: ${location.country}</p>
-                            <p>ICAO-koodi: ${location.icao}</p>
+                            <h3>${airport.name}</h3>
+                            <p>Maa: ${airport.country}</p>
+                            <p>ICAO-koodi: ${airport.icao}</p>
                           </div>`,
             });
 
-            // Lisää markkerille tapahtuma: avaa infoikkuna, kun markkeria klikataan
-               marker.addListener("click", () => {
+            // Lisää markkerille tapahtuma: avaa infoikkuna ja piirrä viiva
+            marker.addListener("click", () => {
                 infoWindow.open(map, marker);
+
+                // Piirretään viiva pelaajan sijainnista valittuun markkeriin
+                const playerLocation = getPlayerLocation(); // Hae pelaajan nykyinen sijainti
+                if (playerLocation) {
+                    drawFlightPath(playerLocation, { lat: airport.lat, lng: airport.lon });
+                } else {
+                    console.error("Pelaajan sijaintia ei löytynyt.");
+                }
             });
         });
     }
 
     // Päivitetään 'valid locations' dataa gameJS.js tiedostosta
-const interval = setInterval(() => {
+    const interval = setInterval(() => {
         if (valid_locations.length > 0) {
-            addMarkers(valid_locations); // Lisäätään markkerit, kun data on saatavilla
+            addMarkers(valid_locations); // Lisätään markkerit, kun data on saatavilla
             clearInterval(interval);
         }
     }, 500); // Tarkitusväli on nyt tässä 500ms
+}
+
+// Funktio pelaajan sijainnin hakemiseen `game_data`-sanakirjasta
+function getPlayerLocation() {
+    if (typeof game_data !== "undefined" && game_data['location_current']) {
+        return {
+            lat: game_data['location_current'].lat,
+            lng: game_data['location_current'].lon,
+        };
+    }
+    return null; // Palautetaan null, jos pelaajan sijainti ei ole määritelty
+}
+
+// Funktio viivan piirtämiseen kahden pisteen välille
+function drawFlightPath(start, end) {
+    // Jos viiva on jo olemassa, poista se ensin
+    if (flightPath) {
+        flightPath.setMap(null);
+    }
+
+    // Luodaan uusi viiva
+    flightPath = new google.maps.Polyline({
+        path: [start, end], // Viivan alku- ja loppupisteet
+        geodesic: true, // Käytetään suoraa linjaa
+        strokeColor: "#FF0000", // Viivan väri
+        strokeOpacity: 1.0, // Viivan läpinäkyvyys
+        strokeWeight: 2, // Viivan paksuus
+    });
+
+    // Näytetään viiva kartalla
+    flightPath.setMap(map);
 }
 
 // Ladataan Google Maps -API ja alustetaan kartta
